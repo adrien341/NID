ca4327493d517a8be2073bc0efde553db8d1faeb
adrien341 <68065056+adrien341@users.noreply.github.com>
2023-10-18 16:54:04 +0200

    Add files via upload

--- streamlite_NID.py
+++ streamlite_NID.py
@@ -1,25 +1,33 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import ast
 
+#to view this Streamlit app on a browser, run it with the following   command: streamlit run streamlite_NID.py
 
 def random_nid(taxon_list):
         return np.random.choice(taxon_list)
 
 # load species.csv
-df = pd.read_csv(r'species.csv')
+df = pd.read_csv(r'species_w2v.csv')
 #print(df)
-taxon_rare = df[df["count"] < 10000]
+
+# warning : Some taxa are over reprensented, hence the artificial threshold here
+taxon_rare = df[df["count"] < 50]
+# creation of a list
 taxon_list = taxon_rare.name.tolist()
-your_pokemon = random_nid(taxon_list)
+your_pokemon = ast.literal_eval(random_nid(taxon_list))[0][0]
 
 st.title("""
-Bienvenue, etudiant.e de la 4e promotion Master NID.
+🎲 Random Taxon 🎲
 """)
-st.write('Cette application te donnera une espèce, ou un groupe taxonomique sur lequel chercher de la donnée, le temps d un exercice')
-submit = st.button("Donne moi un taxon !")
+#st.markdown(''':gray[Made by]''')
+#st.write("[CEEBIOS](https://ceebios.com/)")
+st.write('Cette application tire au sort une espèce, ou un groupe taxonomique.')
+submit = st.button("Donne-moi un taxon !")
 if submit:
    st.header(your_pokemon)
  
 
-st.write("Maintenant, cultive-nous sur ce taxon, en allant sur https://itn5front-y5c3h4ocuq-wl.a.run.app/" )
+st.write("Maintenant, obtenons de la donnée sur ce taxon, en allant sur https://bioinspire-explore.mnhn.fr/" )
+
